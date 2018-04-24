f4a877360dea5a33fa15dafa4b6560b024658f53
vaibhavkedia968 <35337910+vaibhavkedia968@users.noreply.github.com>
2018-04-24 21:22:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ function createTemplate(data){
         </body>
     </html>
     `
+    return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
